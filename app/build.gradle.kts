8811da4bc61de5f7b22c2cad7107470afe4bbd9d
jwkunz <jake.kunzler@gmail.com>
2026-08-29 08:50:27 -0400

    Bump version to 1.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.example.pace_ometer"
         minSdk = 26
         targetSdk = 36
-        versionCode = 11
-        versionName = "1.5"
+        versionCode = 12
+        versionName = "1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
